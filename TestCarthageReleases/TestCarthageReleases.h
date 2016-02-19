7eff386a9b6843806f4491a4311141be03af8535
Bhargav <bhargav.grlnk@gmail.com>
2016-02-19 18:22:46 +0530

    Initial Commit

---
+++ TestCarthageReleases/TestCarthageReleases.h
@@ -0,0 +1,19 @@
+//
+//  TestCarthageReleases.h
+//  TestCarthageReleases
+//
+//  Created by Bhargav Gurlanka on 2/19/16.
+//  Copyright © 2016 Bhargav Gurlanka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestCarthageReleases.
+FOUNDATION_EXPORT double TestCarthageReleasesVersionNumber;
+
+//! Project version string for TestCarthageReleases.
+FOUNDATION_EXPORT const unsigned char TestCarthageReleasesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestCarthageReleases/PublicHeader.h>
+
+

85ac41f422056e4b95837c1ff5dc2683ef58d9bb
Bhargav <bhargav.grlnk@gmail.com>
2016-02-19 18:25:19 +0530

    Add dog that can bark

---
+++ TestCarthageReleases/Dog.swift
@@ -0,0 +1,15 @@
+//
+//  Dog.swift
+//  TestCarthageReleases
+//
+//  Created by Bhargav Gurlanka on 2/19/16.
+//  Copyright © 2016 Bhargav Gurlanka. All rights reserved.
+//
+
+import Foundation
+
+class Dog {
+    func bark() {
+        print("woof woof")
+    }
+}
